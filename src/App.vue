9c4561a501bf5db97a0fc3755c395703dfbde961
daulet <dauka2707@gmail.com>
2024-09-08 03:03:40 +0600

    first commit

---
+++ src/App.vue
@@ -0,0 +1,16 @@
+<script setup>
+import Header from './components/Header.vue'
+</script>
+
+<template>
+  <div class="bg-white w-4/5 m-auto h-screen rounded-xl shadow-xl mt-14">
+    <Header />
+
+    <div>
+      <div>
+        <img src="/like-1.svg" alt="Like 1" />
+        <img src="/sneakers/sneakers-1.jpg" alt="Sneaker" />
+      </div>
+    </div>
+  </div>
+</template>
---
+++ src/components/Header.vue
@@ -0,0 +1,27 @@
+<template>
+  <header class="flex justify-between border-b border-slate-300 px-10 py-8">
+    <div class="flex items-center gap-4">
+      <img src="/logo.png" alt="Logo" class="w-10" />
+      <div>
+        <h2 class="text-xl font-bold uppercase">Vue Sneakers</h2>
+        <p class="text-slate-400">Best sneakers store</p>
+      </div>
+    </div>
+    <ul class="flex gap-10 items-center">
+      <li class="flex gap-3 items-center text-gray-500 hover:text-black cursor-pointer">
+        <img src="/cart.svg" alt="Cart" />
+        <b>8300 KZT</b>
+      </li>
+
+      <li class="flex gap-3 items-center text-gray-500 hover:text-black cursor-pointer">
+        <img src="/heart.svg" alt="Heart" />
+        <span>bookmarks</span>
+      </li>
+
+      <li class="flex gap-3 items-center text-gray-500 hover:text-black cursor-pointer">
+        <img src="/profile.svg" alt="Heart" />
+        <span>Profile</span>
+      </li>
+    </ul>
+  </header>
+</template>
