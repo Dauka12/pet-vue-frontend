2f648ee9eb46d36404e381a1322914f2988b9452
daulet <dauka2707@gmail.com>
2024-09-08 04:21:50 +0600

    asd

--- src/App.vue
+++ src/App.vue
@@ -1,16 +1,15 @@
 <script setup>
-import Header from './components/Header.vue'
+import SneakerCardList from './components/SneakerCardList.vue'
+import SneakerHeader from './components/SneakerHeader.vue'
 </script>
 
 <template>
-  <div class="bg-white w-4/5 m-auto h-screen rounded-xl shadow-xl mt-14">
-    <Header />
+  <div class="bg-white w-4/5 m-auto rounded-xl shadow-xl mt-14">
+    <SneakerHeader />
 
-    <div>
-      <div>
-        <img src="/like-1.svg" alt="Like 1" />
-        <img src="/sneakers/sneakers-1.jpg" alt="Sneaker" />
-      </div>
+    <div class="m-10">
+      <h2 class="font-bold text-3xl mb-8">All sneakers</h2>
+      <SneakerCardList />
     </div>
   </div>
 </template>
---
+++ src/components/SneakerCardList.vue
@@ -0,0 +1,22 @@
+<script setup>
+import SneakerCard from './SneakerCard.vue'
+const onClickAdd = () => {
+  console.log('Add')
+}
+const onClickFavorite = () => {
+  console.log('Favorite')
+}
+</script>
+<template>
+  <div class="grid grid-cols-4 gap-5">
+    <SneakerCard
+      image-url="/sneakers/sneakers-1.jpg"
+      title="Men's sneakers Nike Blazer Mid Suede"
+      :price="6500"
+      :is-favorite="true"
+      :is-added="true"
+      @click-add="onClickAdd"
+      @click-favorite="onClickFavorite"
+    />
+  </div>
+</template>
